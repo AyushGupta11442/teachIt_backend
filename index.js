7cf5571653fc81d5ce5d5e54f90a6dc1b70475f0
Saransh-786 <saranshsaxena01@gmail.com>
2024-07-30 23:53:53 +0530

    signup signin done

--- index.js
+++ index.js
@@ -1,4 +1,23 @@
-// import dotenv from 'dotenv';
-// import conn
-// dotenv.config();
+const express = require('express');
+const mongoose = require('mongoose')
+const authRouter = require("./routes/auth");
+const env = require(env)
 
+
+const PORT = process.env.PORT || 3000;
+const app = express();
+app.use(express.json());
+app.use(authRouter);
+
+
+const DB = "mongodb+srv://saranshcrazysaxena:<password>@cluster0.sb5jks4.mongodb.net/";
+
+mongoose.connect(DB).then(()=>{
+    console.log("connection sucessfull");
+}).catch((e)=>{
+    console.log(e);
+});
+
+app.listen(PORT, "0.0.0.0" , ()=>{
+    console.log(`connected at port ${PORT}`);
+});
